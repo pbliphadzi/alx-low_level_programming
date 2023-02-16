c4c11f02f4133eb64379210b849a1a4423ee8c01
pbliphadzi <pbliphadzi@gmail.com>
2023-02-16 12:19:02 -0800

    lol

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,10 @@
 #include <stdio.h> 
-
 /**
- * main - prints "Programming is like building a multilingual puzzle"
- * return 0.
+ * main - prints "Programming is like building a multilingual puzzle
+ * Return: Always 0
  */
-
 int main(void)
-
 { 
 	puts("\"Programming is like building a multilingual puzzle");
-	
 	return (0);
 }
