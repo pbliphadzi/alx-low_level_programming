300f3c045ed5dbd4c7c67360a29619ce476f7283
pbliphadzi <pbliphadzi@gmail.com>
2023-02-20 03:07:54 -0800

    trailing

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
-#include <stdio.h> 
+#include <stdio.h.
 /**
  * main - prints "Programming is like building a multilingual puzzle
  * Return: Always 0
  */
 int main(void)
-{ 
+{
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
