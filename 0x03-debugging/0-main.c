b30292391bc2ddef4f876a78a8c798b9101b1765
pbliphadzi <pbliphadzi@gmail.com>
2023-02-23 03:43:49 -0800

    jm

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,9 +1,9 @@
 #include "main.h"
-void positive_or_negative(int i);
+
 /**
-* main - tests function that prints if integer is positive or negative
-* Return: 0
-*/
+ * main - tests function that prints if integer is positive or negative
+ * Return: 0
+ */
 
 int main(void)
 {
