8ac8f4423e65c42576114584efe300c9c54e9077
pbliphadzi <pbliphadzi@gmail.com>
2023-03-31 12:18:24 -0700

    fixing bugs for task 1

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -9,12 +9,12 @@
  */
 size_t list_len(const list_t *h)
 {
-	size_t n = 0;
+	size_t nodecounter = 0;
 
-	while (h)
+	while (h != NULL)
 	{
-		n++;
+		nodecounter++;
 		h = h->next;
 	}
-	return (n);
+	return (nodecounter);
 }
