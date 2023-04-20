f1e0ced119c57ab8cce29775ff5d0737192f0799
pbliphadzi <pbliphadzi@gmail.com>
2023-04-20 10:28:10 -0700

    0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,5 +1,5 @@
 #include "main.h"
-#define BUFFER 1024
+#define BUFFER 2048
 
 /**
  * read_textfile - Reads a text file and prints it to POSIX standard output.
@@ -10,40 +10,39 @@
  * Return: If successful, the number of bytes read and printed. Otherwise, 0.
  */
 
-
 ssize_t read_textfile(const char *filename, size_t letters)
 {
+    int filedescip;
+    ssize_t btread, btwrite, tbtwrite;
+    char buffer[BUFFER];
+
     if (filename == NULL)
         return 0;
 
-    FILE *FP = FILEOPEN(filename, "r");
-    if (FP == NULL)
+    filedescip = open(filename, O_RDONLY);
+    if (filedescip == -1)
         return 0;
 
-    char *buffer = malloc(1024);
-    if (buffer == NULL)
-    {
-        FILECLOSE(FP);
-        return 0;
+    tbtwrite = 0;
+    while (letters > 0) {
+        btread = read(filedescip, buffer, BUFFER < letters ? BUFFER : letters);
+        if (btread == -1)
+        {
+            close(filedescip);
+            return 0;
+        }
+        if (btread == 0)
+            break;
+        btwrite = write(STDOUT_FILENO, buffer, btread);
+        if (btwrite == -1)
+        {
+            close(filedescip);
+            return tbtwrite;
+        }
+        tbtwrite += btwrite;
+        letters -= btwrite;
     }
 
-    ssize_t BYTEREAD = FILEREAD(buffer, sizeof(char), 1024, FP);
-    buffer[BYTEREAD] = '\0';
-
-    FILECLOSE(FP);
-
-    if (BYTEREAD == 0)
-    {
-        free(buffer);
-        return 0;
-    }
-
-    ssize_t BYTEWRITE = BYTEWRITE(buffer, sizeof(char), BYTEREAD, stdout);
-    free(buffer);
-
-    if (BYTEWRITE != BYTEREAD)
-        return 0;
-
-    return BYTEREAD;
+    close(filedescip);
+    return tbtwrite;
 }
-
