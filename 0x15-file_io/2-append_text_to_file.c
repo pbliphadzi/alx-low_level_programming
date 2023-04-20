32699cd657d689877a2d9389b22f776a547cd6ec
pbliphadzi <pbliphadzi@gmail.com>
2023-04-20 11:49:35 -0700

    2 update debug

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,19 +1,19 @@
-#include "main.h"
 #include <unistd.h>
 #include <fcntl.h>
+#include <string.h>
 
 int append_text_to_file(const char *filename, char *txcontent)
 {
     int filedescrip;
     ssize_t bywrite;
-
+    
     if (filename == NULL)
         return -1;
-
+    
     filedescrip = open(filename, O_WRONLY | O_APPEND);
     if (filedescrip == -1)
         return -1;
-
+    
     if (txcontent != NULL)
     {
         bywrite = write(filedescrip, txcontent, strlen(txcontent));
@@ -23,7 +23,7 @@ int append_text_to_file(const char *filename, char *txcontent)
             return -1;
         }
     }
-
+    
     close(filedescrip);
     return 1;
 }
