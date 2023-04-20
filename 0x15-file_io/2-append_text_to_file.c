0ba1e13beb176e095c9c5f07a8a5e7329910e742
pbliphadzi <pbliphadzi@gmail.com>
2023-04-20 11:46:50 -0700

    2-append_text_to_file.c

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,33 +1,30 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <sys/stat.h>
+#include "main.h"
+#include <unistd.h>
 #include <fcntl.h>
 
-int create_file(const char *filename, char *text_content) {
-    if (filename == NULL) {
+int append_text_to_file(const char *filename, char *txcontent)
+{
+    int filedescrip;
+    ssize_t bywrite;
+
+    if (filename == NULL)
         return -1;
-    }
-    int fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
-    if (fd == -1) {
+
+    filedescrip = open(filename, O_WRONLY | O_APPEND);
+    if (filedescrip == -1)
         return -1;
-    }
-    if (text_content != NULL) {
-        size_t len = strlen(text_content);
-        char *buf = (char*)malloc(len + 1);
-        if (buf == NULL) {
-            close(fd);
+
+    if (txcontent != NULL)
+    {
+        bywrite = write(filedescrip, txcontent, strlen(txcontent));
+        if (bywrite == -1)
+        {
+            close(filedescrip);
             return -1;
         }
-        strcpy(buf, text_content);
-        ssize_t bytes_written = write(fd, buf, len);
-        free(buf);
-        if (bytes_written == -1) {
-            close(fd);
-            return -1;
-        }
     }
-    close(fd);
+
+    close(filedescrip);
     return 1;
 }
 
