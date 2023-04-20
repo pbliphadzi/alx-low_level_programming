1954d65bc9da5369ecdf563c0332db93ae5a4e51
pbliphadzi <pbliphadzi@gmail.com>
2023-04-20 11:31:03 -0700

    1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,33 +1,38 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include <unistd.h>
 #include <string.h>
-#include <sys/stat.h>
 #include <fcntl.h>
+#include "main.h"
 
-int create_file(const char *filename, char *text_content) {
-    if (filename == NULL) {
-        return -1;
+/**
+ * create_file - creates a file
+ * @filename: the name of the file
+ * @txcont: string to write in the file
+ * Return: 1 On success annd -1 on failure
+ */
+
+int create_file(const char *filename, char *txcont)
+{
+    int filedescrip, len;
+    ssize_t bytes_written;
+
+    if (filename == NULL)
+        return (-1);
+
+    if (txcont == NULL)
+        txcont = "";
+
+    filedescrip = open(filename, O_RDWR | O_TRUNC | O_CREAT, 0600);
+    if (filedescrip == -1)
+        return (-1);
+
+    len = strlen(txcont);
+    bytes_written = write(filedescrip, txcont, len);
+    if (bytes_written == -1)
+    {
+        close(filedescrip);
+        return (0);
     }
-    int fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
-    if (fd == -1) {
-        return -1;
-    }
-    if (text_content != NULL) {
-        size_t len = strlen(text_content);
-        char *buf = (char*)malloc(len + 1);
-        if (buf == NULL) {
-            close(fd);
-            return -1;
-        }
-        strcpy(buf, text_content);
-        ssize_t bytes_written = write(fd, buf, len);
-        free(buf);
-        if (bytes_written == -1) {
-            close(fd);
-            return -1;
-        }
-    }
-    close(fd);
-    return 1;
+
+    close(filedescrip);
+    return (1);
 }
-
