e90190a898ba2c8bb1e4bf6cb6570a8060a00944
pbliphadzi <pbliphadzi@gmail.com>
2023-04-26 02:17:30 -0700

    Attempt t3

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,58 +1,82 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
 
-void fileclose(FILE **file) {
-    if (*file) {
-        fclose(*file);
-        *file = NULL;
-    }
-}
+#define BUFSIZE 1024
 
-int main(int argc, char *argv[]) {
-    char *from_path, *to_path;
-    FILE *from_file, *to_file;
-    char buf[1024];
-    size_t num_read;
+/*
+ * filecopy - copy the content of one file to another file
+ *
+ * Parameters:
+ *   from_path: path to the source file
+ *   to_path: path to the destination file
+ *
+ * Returns: int (0 if success, non-zero if error)
+ */
+int filecopy(const char* from_path, const char* to_path) {
+    int fd_from, fd_to, rd, wr;
+    char buf[BUFSIZE];
 
-    if (argc != 3) {
-        printf("Usage: cp file_from file_to\n");
+    fd_from = open(from_path, O_RDONLY);
+    if (fd_from == -1) {
+        perror(from_path);
         return 1;
     }
-    
-    from_path = argv[1];
-    to_path = argv[2];
-    
-    from_file = fopen(from_path, "rb");
-    if (!from_file) {
-        printf("Error: Can't read from file %s\n", from_path);
+
+    fd_to = open(to_path, O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
+    if (fd_to == -1) {
+        perror(to_path);
+        close(fd_from);
         return 1;
     }
-    
-    to_file = fopen(to_path, "wb");
-    if (!to_file) {
-        printf("Error: Can't write to file %s\n", to_path);
-        fileclose(&from_file);
-        return 1;
-    }
-    
-    while ((num_read = fread(buf, 1, sizeof(buf), from_file)) > 0) {
-        if (fwrite(buf, 1, num_read, to_file) != num_read) {
-            printf("Error: Can't write to file %s\n", to_path);
-            fileclose(&from_file);
-            fileclose(&to_file);
+
+    while ((rd = read(fd_from, buf, BUFSIZE)) > 0) {
+        wr = write(fd_to, buf, rd);
+        if (wr != rd) {
+            perror(to_path);
+            close(fd_from);
+            close(fd_to);
             return 1;
         }
     }
-    
-    if (ferror(from_file)) {
-        printf("Error: Can't read from file %s\n", from_path);
-        fileclose(&from_file);
-        fileclose(&to_file);
+
+    if (rd == -1) {
+        perror(from_path);
+        close(fd_from);
+        close(fd_to);
         return 1;
     }
-    
-    fileclose(&from_file);
-    fileclose(&to_file);
+
+    if (close(fd_from) == -1) {
+        perror(from_path);
+        return 1;
+    }
+
+    if (close(fd_to) == -1) {
+        perror(to_path);
+        return 1;
+    }
+
     return 0;
+}
+
+/*
+ * main - entry point of the program
+ *
+ * Parameters:
+ *   argc: the number of command-line arguments
+ *   argv: an array of strings containing the command-line arguments
+ *
+ * Returns: int (0 if success, non-zero if error)
+ */
+int main(int argc, char **argv) {
+    if (argc != 3) {
+        fprintf(stderr, "Usage: cp file_from file_to\n");
+        return 1;
+    }
+
+    return filecopy(argv[1], argv[2]);
 }
 
