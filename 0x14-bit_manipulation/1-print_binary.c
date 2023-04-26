a97abc30dcde456b9d023d03e2e66b27d8ca2e3d
pbliphadzi <pbliphadzi@gmail.com>
2023-04-26 03:29:00 -0700

    1

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -8,25 +8,26 @@ void print_binary(unsigned long int b)
 {
     if (b == 0) 
     {
-        _putchar('0');
+        putchar('0');
         return;
     }
     
-    int num_bits = sizeof(b) * 8;
-    int found_one = 0;
+    unsigned int num_bits = sizeof(b) * 8;
+    unsigned int found_one = 0;
+    unsigned int i;
     
-    for (int i = num_bits - 1; i >= 0; i--)
+    for (i = num_bits - 1; i >= 0; i--)
     {
         unsigned long int mask = 1UL << i;
         
         if (b & mask)
         {
             found_one = 1;
-            _putchar('1');
+            putchar('1');
         }
         else if (found_one)
         {
-            _putchar('0');
+            putchar('0');
         }
     }
     
