beff92ff68405848fe5b68630d63c2f215c4b4fa
pbliphadzi <pbliphadzi@gmail.com>
2023-04-26 02:05:24 -0700

    3-cp

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -9,25 +9,32 @@ void fileclose(FILE **file) {
 }
 
 int main(int argc, char *argv[]) {
+    char *from_path, *to_path;
+    FILE *from_file, *to_file;
+    char buf[1024];
+    size_t num_read;
+
     if (argc != 3) {
         printf("Usage: cp file_from file_to\n");
         return 1;
     }
-    char *from_path = argv[1];
-    char *to_path = argv[2];
-    FILE *from_file = fopen(from_path, "rb");
+    
+    from_path = argv[1];
+    to_path = argv[2];
+    
+    from_file = fopen(from_path, "rb");
     if (!from_file) {
         printf("Error: Can't read from file %s\n", from_path);
         return 1;
     }
-    FILE *to_file = fopen(to_path, "wb");
+    
+    to_file = fopen(to_path, "wb");
     if (!to_file) {
         printf("Error: Can't write to file %s\n", to_path);
         fileclose(&from_file);
         return 1;
     }
-    char buf[1024];
-    size_t num_read;
+    
     while ((num_read = fread(buf, 1, sizeof(buf), from_file)) > 0) {
         if (fwrite(buf, 1, num_read, to_file) != num_read) {
             printf("Error: Can't write to file %s\n", to_path);
@@ -36,12 +43,14 @@ int main(int argc, char *argv[]) {
             return 1;
         }
     }
+    
     if (ferror(from_file)) {
         printf("Error: Can't read from file %s\n", from_path);
         fileclose(&from_file);
         fileclose(&to_file);
         return 1;
     }
+    
     fileclose(&from_file);
     fileclose(&to_file);
     return 0;
