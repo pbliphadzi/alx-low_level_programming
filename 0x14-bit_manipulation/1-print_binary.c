20503c6114dfcb9e599ccf66ca73d00969bc7cf0
pbliphadzi <pbliphadzi@gmail.com>
2023-07-06 10:51:20 -0700

    Bit Manipulations submission

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * print_binary - prints the binary representation of a number
+ * @n: unsigned long integer
+ */
+void print_binary(unsigned long int n)
+{
+	unsigned long int bit = 1UL << ((sizeof(unsigned long int) * 8) - 1);
+	int has_printed = 0;
+
+	if (n == 0)
+	{
+		_putchar('0');
+		return;
+	}
+
+	while (bit > 0)
+	{
+		if ((n & bit) == bit)
+		{
+			_putchar('1');
+			has_printed = 1;
+		}
+		else if (has_printed == 1)
+		{
+			_putchar('0');
+		}
+		bit = bit >> 1;
+	}
+}
