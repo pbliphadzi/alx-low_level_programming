432f09ae22d250a27f3c1b597f45fed19062dae6
pbliphadzi <pbliphadzi@gmail.com>
2023-07-27 05:17:10 -0700

    submission errors

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -4,75 +4,97 @@
 #include <fcntl.h>
 #include <unistd.h>
 
-#define BUFFER_SIZE 1024
-
 /**
- * copyFile - copies the content of one file to another
- * @fileFrom: the source file
- * @fileTo: the destination file
+ * check_arguments - checks for the correct number of arguments
+ * @argc: number of arguments
+ *
  * Return: void
  */
-void copyFile(const char *fileFrom, const char *fileTo)
+void check_arguments(int argc)
 {
-    int fdFrom = open(fileFrom, O_RDONLY);
-    int fdTo = open(fileTo, O_WRONLY | O_CREAT | O_TRUNC, 0666);
-    char buffer[BUFFER_SIZE];
-    ssize_t bytesRead, bytesWritten;
-
-    if (fdFrom == -1)
-    {
-        dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", fileFrom);
-        exit(98);
-    }
-    if (fdTo == -1)
-    {
-        dprintf(STDERR_FILENO, "Error: Can't write to %s\n", fileTo);
-        exit(99);
-    }
-
-    while ((bytesRead = read(fdFrom, buffer, BUFFER_SIZE)) > 0)
-    {
-        bytesWritten = write(fdTo, buffer, bytesRead);
-        if (bytesWritten == -1)
-        {
-            dprintf(STDERR_FILENO, "Error: Can't write to %s\n", fileTo);
-            exit(99);
-        }
-    }
-
-    if (bytesRead == -1)
-    {
-        dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", fileFrom);
-        exit(98);
-    }
-
-    if (close(fdFrom) == -1 || close(fdTo) == -1)
-    {
-        dprintf(STDERR_FILENO, "Error: Can't close file descriptors\n");
-        exit(100);
-    }
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
 }
 
 /**
- * main - entry point
- * @argc: argument count
- * @argv: argument vector
- * Return: 0 on success, other values on failure
+ * check_io - checks for I/O errors and closes file descriptors if necessary
+ * @result: result of an I/O operation
+ * @file: file name associated with the operation
+ * @fd_from: file descriptor of file_from, or -1
+ * @fd_to: file descriptor of file_to, or -1
+ *
+ * Return: void
+ */
+void check_io(ssize_t result, char *file, int fd_from, int fd_to)
+{
+	if (result == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't process %s\n", file);
+		if (fd_from != -1)
+			close(fd_from);
+		if (fd_to != -1)
+			close(fd_to);
+		exit(EXIT_FAILURE);
+	}
+}
+
+/**
+ * check_close - checks that file descriptors were closed properly
+ * @result: result of the close operation
+ * @fd: file descriptor
+ *
+ * Return: void
+ */
+void check_close(int result, int fd)
+{
+	if (result == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
+		exit(EXIT_FAILURE);
+	}
+}
+
+/**
+ * main - copies the content of a file to another file.
+ * @argc: number of arguments passed
+ * @argv: array of pointers to the arguments
+ *
+ * Return: 0 on success
  */
 int main(int argc, char *argv[])
 {
-    const char *fileFrom, *fileTo;
+	int fd_from, fd_to;
+	ssize_t len_read, len_written;
+	char buffer[1024];
+	mode_t file_perm;
 
-    if (argc != 3)
-    {
-        dprintf(STDERR_FILENO, "Usage: %s file_from file_to\n", argv[0]);
-        return 97;
-    }
+	check_arguments(argc);
 
-    fileFrom = argv[1];
-    fileTo = argv[2];
-    copyFile(fileFrom, fileTo);
+	fd_from = open(argv[1], O_RDONLY);
+	check_io((ssize_t)fd_from, argv[1], -1, -1);
 
-    return 0;
+	file_perm = S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH;
+	fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, file_perm);
+	check_io((ssize_t)fd_to, argv[2], fd_from, -1);
+
+	len_read = 1024;
+	while (len_read == 1024)
+	{
+		len_read = read(fd_from, buffer, 1024);
+		check_io(len_read, argv[1], fd_from, fd_to);
+
+		len_written = write(fd_to, buffer, len_read);
+		if (len_written != len_read)
+			len_written = -1;
+		check_io(len_written, argv[2], fd_from, fd_to);
+	}
+
+	check_close(close(fd_to), fd_to);
+	check_close(close(fd_from), fd_from);
+
+	return (0);
 }
 
